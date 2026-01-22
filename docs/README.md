ee0f5c8e3bef56d66548cef65520050e1bd92dea
lang-jiaqi <E1481847@u.nus.edu>
2026-01-22 16:40:03 +0800

    finish level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Doraemon User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Doraemon.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Doraemon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Doraemon\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+
+    }
+}
