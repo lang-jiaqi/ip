a378d203af5bfcab23200110ff3c10f13edf85a1
lang-jiaqi <E1481847@u.nus.edu>
2026-01-22 16:52:05 +0800

    finish level-1

--- src/main/java/Doraemon.java
+++ src/main/java/Doraemon.java
@@ -2,12 +2,27 @@ import java.util.Scanner;
 public class Doraemon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Doraemon\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
-
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Doraemon\n    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        Boolean end = false;
+        while (!end) {
+            String input = sc.nextLine();
+            if(input.equals("bye")){
+                end = true;
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+            }
+            else if(input.equals("list")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    list");
+                System.out.println("    ____________________________________________________________");
+            }
+            else if(input.equals("blah")){
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    blah");
+                System.out.println("    ____________________________________________________________");
+            }
+        }
     }
 }
