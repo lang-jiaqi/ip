b0c661fe57105b30e05e57d0d5f0c71474fd7424
lang-jiaqi <E1481847@u.nus.edu>
2026-01-28 23:23:08 +0800

    Add complete save-to-disk feature

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -12,14 +12,18 @@ public class Storage {
         this.filePath = filePath;
     }
 
-    public void save(ArrayList<Task> tasks) {
+    /**
+     * Save the tasks to the disk, if the folder doesn't exist,create a folder.
+     * @param tasks
+     */
+    public void saveAll(ArrayList<Task> tasks) {
         try{
-            File folder = new File("./data");
-            if(!folder.exists()){
-                folder.mkdir();
+            File file = new File(filePath);
+            if (!file.getParentFile().exists()){
+                file.getParentFile().mkdir();
             }
-            FileWriter fw = new FileWriter("./data/Doraemon.txt", true);
-            for (Task task : tasks) {
+            FileWriter fw = new FileWriter("./data/Doraemon.txt");
+            for (Task task : tasks){
                 fw.write(task.toFileFormat() + System.lineSeparator());
             }
             fw.close();
@@ -29,22 +33,50 @@ public class Storage {
 
     }
 
-    public void loadFromFile(ArrayList<Task> tasks) {
-        File file = new File("./data/Doraemon.txt");
+    /**
+     * load the tasks from the disk, if the file doesn't exist, just return.
+     * @return an arraylist of asks
+     */
+    public ArrayList<Task> loadFromFile() {
+        ArrayList<Task> loadedTasks = new ArrayList<>();
+        File file = new File(filePath);
         if(!file.exists()){
-            return;
+            return loadedTasks;
         }
 
         try{
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
+                if(line.isEmpty()){
+                    continue;
+                }
                 String[] parts = line.split(" \\| ");
+                Task t = null;
+
+                switch (parts[0]) {
+                    case "T":
+                        t = new ToDo(parts[2]);
+                        break;
+                    case "D":
+                        t = new Deadline(parts[2],parts[3]);
+                        break;
+                    case "E":
+                        t = new Event(parts[2],parts[3],parts[4]);
+                        break;
+                }
+                if (t!= null) {
+                    if(parts[1].equals("1")){
+                        t.markAsDone();
+                    }
+                    loadedTasks.add(t);
+                }
                 String type = parts[0];
                 boolean  isDone = parts[1].equals("1");
             }
         } catch (IOException e){
             System.out.println("loading failed" + e.getMessage());
         }
+        return loadedTasks;
     }
 }
