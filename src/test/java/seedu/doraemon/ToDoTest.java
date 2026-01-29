87eabef460b6ea5a8901aa25bf0dfd555481104e
lang-jiaqi <E1481847@u.nus.edu>
2026-01-29 21:11:20 +0800

    Add Junits for Storage and ToDO classes

---
+++ src/test/java/seedu/doraemon/ToDoTest.java
@@ -0,0 +1,28 @@
+package seedu.doraemon;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoTest {
+    @Test
+    public void testToString_newToDo_correctFormat() {
+        ToDo todo = new ToDo("read book");
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void test_changeStatus_iconChanges() {
+        ToDo todo = new ToDo("read book");
+        todo.markAsDone();
+        assertEquals("[X]", todo.getSatusIcon());
+        assertEquals("[T][X] read book", todo.toString());
+        todo.Unmark();
+        assertEquals("[T][ ] read book", todo.toString());
+    }
+
+    @Test
+    public void testToFileFormat_correctData() {
+        ToDo todo = new ToDo("run marathon");
+        assertEquals("T | 0 | run marathon", todo.toFileFormat());
+    }
+}
