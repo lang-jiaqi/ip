50e75f260441f32b35ca1ceff4e3513c01e354d3
lang-jiaqi <E1481847@u.nus.edu>
2026-02-19 17:06:49 +0800

    Add more JUnit tests

--- src/test/java/seedu/doraemon/ToDoTest.java
+++ src/test/java/seedu/doraemon/ToDoTest.java
@@ -1,8 +1,10 @@
 package seedu.doraemon;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
 public class ToDoTest {
     @Test
     public void testToString_newToDo_correctFormat() {
@@ -11,18 +13,56 @@ public class ToDoTest {
     }
 
     @Test
-    public void test_changeStatus_iconChanges() {
+    public void testToString_markedToDo_correctFormat() {
         ToDo todo = new ToDo("read book");
         todo.markAsDone();
+        assertEquals("[T][X]read book", todo.toString());
+    }
+
+    @Test
+    public void test_changeStatus_iconChanges() {
+        ToDo todo = new ToDo("read book");
+        assertFalse(todo.isDone());
+        assertEquals("[ ]", todo.getSatusIcon());
+        todo.markAsDone();
+        assertTrue(todo.isDone());
         assertEquals("[X]", todo.getSatusIcon());
         assertEquals("[T][X]read book", todo.toString());
-        todo.Unmark();
+        todo.unMark();
+        assertFalse(todo.isDone());
+        assertEquals("[ ]", todo.getSatusIcon());
         assertEquals("[T][ ]read book", todo.toString());
     }
 
     @Test
-    public void testToFileFormat_correctData() {
+    public void testToFileFormat_newToDo_correctFormat() {
         ToDo todo = new ToDo("run marathon");
         assertEquals("T | 0 | run marathon", todo.toFileFormat());
+    }
+
+    @Test
+    public void testToFileFormat_markedToDo_correctFormat() {
+        ToDo todo = new ToDo("run marathon");
+        todo.markAsDone();
+        assertEquals("T | 1 | run marathon", todo.toFileFormat());
+    }
+
+    @Test
+    public void testGetDescription_returnsCorrectDescription() {
+        ToDo todo = new ToDo("buy groceries");
+        assertEquals("buy groceries", todo.getDescription());
+    }
+
+    @Test
+    public void testIsDone_newToDo_returnsFalse() {
+        ToDo todo = new ToDo("test task");
+        assertFalse(todo.isDone());
+    }
+
+    @Test
+    public void testIsDone_markedToDo_returnsTrue() {
+        ToDo todo = new ToDo("test task");
+        todo.markAsDone();
+        assertTrue(todo.isDone());
     }
 }
